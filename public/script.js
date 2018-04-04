98f16c83a434056358fba3bc607a70cc5e7fa5a7
Your Name <you@example.com>
2018-04-04 21:01:13 +0400

    somem changes with files

---
+++ public/script.js
@@ -0,0 +1,144 @@
+var side = 15;
+var gr;
+var grassArr = [];
+var xotakerArr = [];
+var gishatichArr = [];
+var amenakerArr  = [];
+var gortArr = [];
+var count = 400, xotakcount = 100, gishcount = 20, amenakercount = 20, gortcount = 10;
+var matrix = new Array(50);
+for (var i = 0; i < matrix.length; i++) {
+    matrix[i] = new Array(50);
+    for (var j = 0; j < matrix[i].length; j++)
+        matrix[i][j] = 0;
+}
+while (count > 0) {
+    tx = Math.floor(Math.random() * 50);
+    ty = Math.floor(Math.random() * 50);
+    if (matrix[tx][ty] == 0) {
+        gr = new Grass(tx, ty);
+        grassArr.push(gr);
+        count--;
+    }
+}
+while (xotakcount > 0) {
+    tx = Math.floor(Math.random() * 50);
+    ty = Math.floor(Math.random() * 50);
+    if (matrix[tx][ty] == 0) {
+        gr = new Xotaker(tx, ty);
+        xotakerArr.push(gr);
+        xotakcount--;
+    }
+}
+while (gishcount > 0) {
+    tx = Math.floor(Math.random() * 50);
+    ty = Math.floor(Math.random() * 50);
+    if (matrix[tx][ty] == 0) {
+        gr = new Gishatich(tx, ty);
+        gishatichArr.push(gr);
+        gishcount--;
+    }
+}
+while (amenakercount > 0) {
+    tx = Math.floor(Math.random() * 50);
+    ty = Math.floor(Math.random() * 50);
+    if (matrix[tx][ty] == 0) {
+        gr = new Amenaker(tx, ty);
+        amenakerArr.push(gr);
+        amenakercount--;
+    }
+}
+while (gortcount > 0) {
+    tx = Math.floor(Math.random() * 50);
+    ty = Math.floor(Math.random() * 50);
+    if (matrix[tx][ty] == 0) {
+        gr = new Gort(tx, ty);
+        gortArr.push(gr);
+        gortcount--;
+    }
+}
+function setup() {
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+}
+
+function draw() {
+    background('aqua');
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+                rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+                rect(x * side, y * side, side, side);
+            }
+            else if(matrix[y][x] == 4)
+            {
+                fill("black");
+                rect(x * side, y * side, side, side);
+            }
+            else if(matrix[y][x] == 5)
+            {
+                fill("blue");
+                rect(x * side, y * side, side, side);                
+            }
+        }
+    }
+
+
+
+    for (var i = 0; i < grassArr.length; i++) {
+        grassArr[i].id = i;
+        grassArr[i].bazmanal();
+        grassArr[i].tunavorvel();
+    }
+    var utelch;
+    for (var i = 0; i < xotakerArr.length; i++) {
+        xotakerArr[i].id = i;
+        utelch = xotakerArr[i].utel();
+        if (!utelch)
+            xotakerArr[i].sharjvel();
+        xotakerArr[i].bazmanal();
+        xotakerArr[i].mahanal();
+    }
+    for (var i = 0; i < gishatichArr.length; i++) {
+        gishatichArr[i].id = i;
+        utelch = gishatichArr[i].utel();
+        if(!utelch)
+        {
+          gishatichArr[i].sharjvel();
+          gishatichArr[i].energy--;
+        }
+  
+        gishatichArr[i].bazmanal();
+        gishatichArr[i].mahanal();
+    }
+    for(var i = 0; i < amenakerArr.length; i++)
+    {  
+        amenakerArr[i].id = i; 
+        utelch = amenakerArr[i].utel();
+        if(!utelch)
+        {
+            amenakerArr[i].sharjvel();
+            amenakerArr[i].energy--;
+        }
+        amenakerArr[i].bazmanal();
+        amenakerArr[i].mahanal();
+    }
+    for(var i = 0; i < gortArr.length; i++)
+    {
+        gortArr[i].id = i;
+        gortArr[i].sharjvel();
+        gortArr[i].tunavorel();
+        //gortArr[i].mahanal(); balance em pahpanum, ok?
+    }
+}
+
